280b47d9f095b46aa3530311425f3e6512cb2b99
alok0423 <ishuu379@gmail.com>
2025-12-22 15:14:26 +0530

    moved to folder

--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const BRAND_LEFT = '#0A1B64'
-export const BRAND_RIGHT = '#0D47A1'
-export const ACCENT = '#F5C400'
--- src/utils/format.js
+++
@@ -1,5 +0,0 @@
-export const formatNumber = (n) => {
-  if(n >= 1000000) return (n/1000000).toFixed(1)+'M'
-  if(n >= 1000) return (n/1000).toFixed(1)+'k'
-  return n.toString()
-}
